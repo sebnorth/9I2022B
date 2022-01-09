ff1adc886bf3bab6b2b9d790089b9af7ec828795
Sebastian Agata <sebnorth@gmail.com>
2022-01-09 12:12:04 +0100

    Dodaj pliki projektów.

---
+++ Wizytowka/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wizytowka
+{
+    struct Pracownik {
+
+        public string imie;
+        public string nazwisko;
+        public string email;
+        public int telefon;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pracownik p;
+
+            Console.WriteLine("Podaj imie: ");
+            p.imie = Console.ReadLine();
+
+            Console.WriteLine("Podaj nazwisko: ");
+            p.nazwisko = Console.ReadLine();
+
+            Console.WriteLine("Podaj email: ");
+            p.email = Console.ReadLine();
+
+            Console.WriteLine("Podaj numer telefonu: ");
+            p.telefon = Convert.ToInt32(Console.ReadLine());
+
+            /*
+             * obserwacja: długość najdłuższego napisu + 4 = długość wizytówki
+             * 1 połączyć imię z nazwiskiem i jedną spacją -> zmienna imienazwisko
+             * 2 połączyć email z napisem "email"
+             * 3 połączyć numer telefonu z napisem "tel. "
+             * 4 obliczyć długości a = dlugosć napisu imienazwisko,b,c tych trzech napisów
+             * 5 wybrać największą z tych długości -> maksimum
+             * 6 nowa zmienna dlugoscWizytowki = maksimum + 4
+             * 7 na przykład ilość spacji po obu stronach napisu imienazwisko obliczamy:
+             * (dlugoscWizytowki - a)/2
+             * 8 uwaga. Może powstać problem z niedokładnym wycentrowaniem z powodu błędu 
+             * zaokrąglenia przy dzieleniu przez 2
+             */
+
+            string imienazwisko = p.imie + " " + p.nazwisko; // 1
+            p.email = "e-mail:" + p.email; // 2
+            string telefon = "tel. " + p.telefon; // 3
+
+            int a = imienazwisko.Length;
+            int b = p.email.Length;
+            int c = telefon.Length; // 4
+            int maksimum = Math.Max(Math.Max(a, b), c); // 5
+            int dlugoscWizytowki = maksimum + 4; // 6
+
+            // pętla rysująca gwiazdki
+            for (int i = 0; i < dlugoscWizytowki; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+            // 1 gwiazdka
+            Console.Write("*");
+            // seria spacji
+            for (int i = 0; i < (dlugoscWizytowki - a - 2) / 2; i++)
+            {
+                Console.Write(" ");
+            }
+
+            // imienazwisko
+            Console.Write(imienazwisko);
+            // seria spacji
+            for (int i = 0; i < (dlugoscWizytowki - a - 2) / 2; i++)
+            {
+                Console.Write(" ");
+            }
+            // 1 gwiazdka
+            Console.Write("*");
+            Console.WriteLine();
+
+            //Console.WriteLine(p.imie);
+            //Console.WriteLine(p.nazwisko);
+            //Console.WriteLine(p.email);
+            //Console.WriteLine(p.telefon);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Wizytowka/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("Wizytowka")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Wizytowka")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("17f69dcd-0bb1-4589-bd20-7a58a9fd2d64")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
